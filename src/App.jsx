c3c65574d76854f4700f98399fb7be18bcb22a74
Vladimir <gerov.vladimir@gmail.com>
2024-07-07 20:34:01 +0200

    going back to static layout in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@
 // Your components go here
 
 
-// import SingleCard from "./Components/SingleDataCard";
-// import Student from "./Components/Student";
-// import Hero from "./Components/Hero";
-// import Header from "./Components/Header";
-// import Footer from "./Components/Footer";
-import Home from "./Pages/Home";
+import SingleCard from "./Components/SingleDataCard";
+import Student from "./Components/Student";
+import Hero from "./Components/Hero";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+// import Home from "./Pages/Home";
 import "./index.css";
 
 
 const App = () => {
   return <div className="flex flex-col items-center justify-between h-screen ">
-    <Home />
-    {/* < Header /> */}
-    {/* < Hero /> */}
-    {/* < Student /> */}
-    {/* < SingleCard /> */}
-    {/* < Footer /> */}
+
+    < Header /> */}
+    < Hero />
+    < Student />
+    < SingleCard />
+    < Footer />
     </div>;
 };
 
